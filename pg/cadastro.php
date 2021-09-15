a91e9947276278838207e476434291825d16de9a
brpgit2020 <bruno2144@gmail.com>
2021-09-15 17:36:54 -0300

    added the label date

---
+++ pg/cadastro.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página de Cadastro - Loja BRP</title>
+    <link rel="stylesheet" href="../css/cadastro.css">
+</head>
+<body>
+    <header id="cabecalho">
+        <h1 id="titulo">
+            Cadastro
+        </h1>
+        <h2 id=subtitulo>
+            Bem-vindo! É aqui que você efetua o seu cadastro na loja virtual!
+        </h2>
+    </header>
+    <section id="cadastro">
+       <form action="#" name="fcadastro" id="fcadastro" method="post">
+           <label name="lbname" id="lbname">Nome completo: </label>
+           <input type="text" name="nome" id="iname" maxlength="50" size="60">
+           <label nome="lbendereco" id="lbendereco">Endereço: </label>
+           <input type="text" name="endereco" id="inendereco" maxlength="50" size="60">
+           <label nome="lbairro" id="lbairro">Bairro: </label>
+           <input type="text" name="bairro" id="inbairro" maxlength="50" size="60">
+           <label nome="lbdate" id="lbdate">Data de nascimento</label>
+       </form>
+    </section>
+    <footer id="roda-pe">
+
+    </footer>
+</body>
+</html>
